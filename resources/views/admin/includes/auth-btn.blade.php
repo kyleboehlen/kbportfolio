d85f2898e4d5fa61714f348ff5c2db4d28d72f5e
kyleboehlen <kyleboehlen@gmail.com>
2021-07-07 17:00:54 -0600

    Fix auth button on mobile

--- resources/views/admin/includes/auth-btn.blade.php
+++ resources/views/admin/includes/auth-btn.blade.php
@@ -1,10 +1,10 @@
 @if(\Auth::check())
-    <button class="btn btn-outline-success fs-3 auth-hover-hide" type="button">Admin</button>
-    <form class="auth-hover-show" action="{{ route('logout') }}" method="POST">
+    <form action="{{ route('logout') }}" method="POST">
         @csrf
-        <button class="btn btn-danger fs-3" type="submit">Log Out</button>
+        <button class="btn btn-outline-success fs-3 auth-hover-hide" type="submit">Admin</button>
+        <button class="btn btn-danger fs-3 auth-hover-show" type="submit">Log Out</button>
     </form>
 @else
-    <button class="btn btn-outline-danger fs-3 auth-hover-hide" type="button">Guest</button>
+    <a href="{{ route('login') }}" class="btn btn-outline-danger fs-3 auth-hover-hide">Guest</a>
     <a class="btn btn-success fs-3 auth-hover-show" href="{{ route('login') }}">Login</a>
 @endif
