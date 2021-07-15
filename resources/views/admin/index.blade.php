f63ec1ae15e7e87f6c1c1d5d8d5225bdf8e4df65
kyleboehlen <kyleboehlen@gmail.com>
2021-07-15 11:37:31 -0600

    Added xkcd comic to the admin home

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,8 @@
 @extends('admin.base')
 
 @section('panel')
-    <div class="panel full-height placeholder d-flex justify-content-center align-items-center">
+    <div class="panel full-height placeholder d-flex flex-column justify-content-evenly align-items-center">
         <h2 class="home-header">No place like 127.0.0.1 :)</h2>
+        <img class="d-none d-lg-block" src="https://imgs.xkcd.com/comics/computer_problems.png" />
     </div>
 @endsection
