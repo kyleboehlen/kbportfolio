eda23f4319be1bf7391babcbf96dee9cf07d759e
Kyle <kyleboehlen@gmail.com>
2022-08-12 16:05:29 -0600

    Revert database config changes, update more aws deployment shit

--- .platform/hooks/postdeploy/04_build_mix.sh
+++ .platform/hooks/postdeploy/04_build_mix.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
 # Build NPM mix
-
-# sudo npm run prod
 npm -v
-# sudo touch public/mix-manifest.json
-# sudo chmod -R 777 public
-# npm run prod
-# sudo chmod -R 775 public
-# sudo chown -R webapp:webapp public
+npm run prod
