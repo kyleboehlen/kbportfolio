19b71e48ef7b26d01ebf497c82eb91b013083337
Kyle <kyleboehlen@gmail.com>
2022-12-28 14:43:15 -0700

    Fix old storage reference to use s3

--- resources/views/admin/includes/no-shoots-msg.blade.php
+++ resources/views/admin/includes/no-shoots-msg.blade.php
@@ -1,2 +1,2 @@
-<img src="/storage/icons/no-camera.png" class="rounded mx-auto d-block mt-5 mb-3" />
+<img src="{{ Storage::url(config('filesystems.dir.icons')) . 'no-camera.png' }}" class="rounded mx-auto d-block mt-5 mb-3" />
 <h2 class="display-5 text-center mt-3">No shoots found.</h2>
