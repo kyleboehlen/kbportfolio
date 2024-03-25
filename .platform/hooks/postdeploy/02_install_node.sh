17901ffaef56fc705919cb3865f8f5d43e6c4105
Kyle Boehlen <31298062+kyleboehlen@users.noreply.github.com>
2024-03-24 23:21:33 -0600

    Update 02_install_node.sh

--- .platform/hooks/postdeploy/02_install_node.sh
+++ .platform/hooks/postdeploy/02_install_node.sh
@@ -2,5 +2,6 @@
 
 # Install Node
 sudo yum install -y gcc-c++ make
+yum --enablerepo=extras install -y epel-release
 curl --silent --location https://rpm.nodesource.com/setup_18.x | bash -
 sudo yum install -y nodejs --enablerepo=nodesource
