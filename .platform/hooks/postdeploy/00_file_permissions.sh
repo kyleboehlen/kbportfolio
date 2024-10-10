1dc62d31f877ce15c183471d246754c4b550bd20
Kyle Boehlen <kyleboehlen@gmail.com>
2024-10-10 08:57:53 -0600

    fix logging

--- .platform/hooks/postdeploy/00_file_permissions.sh
+++ .platform/hooks/postdeploy/00_file_permissions.sh
@@ -3,3 +3,10 @@
 sudo chmod -R 775 storage/
 sudo chmod -R 775 bootstrap/cache/
 sudo chmod -R 775 public/
+
+touch storage/logs/laravel.log
+php artisan config:cache
+sudo chown webapp:ec2-user storage/logs/laravel.log
+sudo chown -R webapp:ec2-user /var/app/current
+sudo chmod -R 775 storage/
+sudo chmod 777 storage/logs/laravel.log
